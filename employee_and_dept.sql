25e5c2c0fc74c1f38bd5ffaf5d7d43626ff90b2a
Asep Arifyan <aseparifyan@gmail.com>
2021-03-30 11:08:02 +0700

    Update and rename employee.sql to employee_and_dept.sql

--- employee_and_dept.sql
+++ employee_and_dept.sql
@@ -5,3 +5,9 @@ CREATE TABLE [dbo].[Employee](
 	[Salary] [int] NULL,
 	[DeptId] [int] NULL
 ) ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[Department](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](60) NULL
+) 
