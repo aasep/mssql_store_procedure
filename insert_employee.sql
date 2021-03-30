c85176a6cd168d2e3d72e7d7170bd5e00b8b922d
Asep Arifyan <aseparifyan@gmail.com>
2021-03-30 11:07:00 +0700

    Add files via upload

---
+++ insert_employee.sql
@@ -0,0 +1,24 @@
+insert into Employee values ('Sailor','Male',200,1);
+insert into Employee values ('Ricky','Male',300,1);
+insert into Employee values ('Martin','Male',400,1);
+insert into Employee values ('Alex','Male',500,1);
+insert into Employee values ('John','Male',600,1);
+insert into Employee values ('Dustin','Male',600,1);
+insert into Employee values ('Rena','Female',700,1);
+insert into Employee values ('Sarah','Female',800,1);
+insert into Employee values ('Alisha','Female',200,1);
+insert into Employee values ('Elena','Female',300,1);
+
+insert into Employee values ('Bravo','Female',300,5);
+insert into Employee values ('Samuel','Male',300,5);
+
+insert into Employee values ('Jasmine','Female',400,6);
+insert into Employee values ('Bram','Male',400,6);
+
+insert into Department values ('HR');
+insert into Department values ('IT');
+insert into Department values ('Finance');
+insert into Department values ('Operation');
+
+
+
