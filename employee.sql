096d4549081ca1a6d2aeb69d082468aa8d9f82ad
Asep Arifyan <aseparifyan@gmail.com>
2021-03-30 11:04:18 +0700

    Add files via upload

---
+++ employee.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Employee](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](60) NULL,
+	[Gender] [varchar](10) NULL,
+	[Salary] [int] NULL,
+	[DeptId] [int] NULL
+) ON [PRIMARY]
